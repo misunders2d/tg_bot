e04b8c376eb14fb69ec2974dc4f16a08acf2a3a6
MISUNDERSTOOD <2djohar@gmail.com>
2023-06-14 15:54:02 +0300

    removing reruns

--- main_tg.py
+++ main_tg.py
@@ -170,7 +170,7 @@ if 'running' not in st.session_state:
     dp.add_handler(CommandHandler("stop", stop))
     dp.add_handler(CommandHandler("create", create))
     dp.add_handler(MessageHandler(Filters.text, reply))
-    dp.add_error_handler(error)
+    # dp.add_error_handler(error)
 
     # Start the bot
     updater.start_polling()
