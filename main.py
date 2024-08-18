97a4e4a6645224dec9d3612c8b8eebe0edcc5e27
MISUNDERSTOOD <2djohar@gmail.com>
2024-08-18 20:09:41 +0300

    working on image vision

--- main.py
+++ main.py
@@ -1,48 +1,125 @@
 from dotenv import load_dotenv
 load_dotenv('.env')
 
-import os, time
-from deta import Deta
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
+import os, requests, pickle
+from io import BytesIO
+from typing import Final
+
+from deta import Deta
 from openai import OpenAI
 
 import workers
 
-ASSISTANT_ID = os.getenv('ASSISTANT_ID')
-OPENAI_KEY = os.getenv('AI_KEY')
-DETA_ID = os.getenv('DETA_KEY')
-print(OPENAI_KEY)
+ASSISTANT_ID: Final = os.getenv('ASSISTANT_ID')
+OPENAI_KEY: Final = os.getenv('AI_KEY')
+TG_KEY: Final = os.getenv('TG_KEY')
+BOT_HANDLE = '@my_temp_bot_for_testing_bot'
+DETA_ID: Final = os.getenv('DETA_KEY')
 
-# CHAT_ID = '107'
+client = OpenAI(api_key = OPENAI_KEY)
 
-# client = OpenAI(api_key = OPENAI_KEY)
+deta_base = Deta(DETA_ID)
+# client.beta.threads.delete('thread_fSravSQAjuQw0HwKQMjeKKmH')
 
-# deta_base = Deta(DETA_ID)
-# # client.beta.threads.delete('thread_Y1WzIYB3uqTXYSWwRpY6wYzi')
+current_sessions = {}
 
-# if not (chat_thread:=workers.check_thread(CHAT_ID, deta_base)):
-#     chat_thread = client.beta.threads.create()
-#     workers.push_thread(CHAT_ID, chat_thread.id, deta_base)
-# current_thread = client.beta.threads.retrieve(thread_id = chat_thread)
-# print(current_thread)
+# Command to provide help information
+async def assist_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('Here comes the help')
 
-# text_input = 'Hey, who are you?'
-# messages = [{'type':'text','text':text_input}]
-# _ = client.beta.threads.messages.create(
-#     thread_id = current_thread.id,
-#     content = messages,
-#     role = 'user'
-#     )
-# current_run = client.beta.threads.runs.create(
-#     thread_id=chat_thread,
-#     assistant_id=ASSISTANT_ID
-#     )
-# current_status = 'queued'
-# while current_status in ('queued','in_progress'):
-#     time.sleep(0.5)
-#     current_run = client.beta.threads.runs.retrieve(thread_id = current_thread.id, run_id = current_run.id)
-#     current_status = current_run.status
-#     print(current_status)
-# messages = client.beta.threads.messages.list(current_thread.id)
-# print(messages)
-# # # response = messages.data[0].content[0].text.value
+def retrieve_thread(chat_id):
+    if chat_id not in current_sessions:
+        if not (chat_thread_id:=workers.check_thread(chat_id, deta_base)):
+            chat_thread = client.beta.threads.create()
+            chat_thread_id = chat_thread.id
+            workers.push_thread(chat_id, chat_thread_id, deta_base)
+        current_thread = client.beta.threads.retrieve(thread_id = chat_thread_id)
+        current_sessions.update({chat_id:current_thread})
+    else:
+        current_thread = current_sessions.get(chat_id)
+    return current_thread
+
+def generate_response(user_input: str, current_thread: str, voice: bool = False) -> str:
+    normalized_input: str = user_input.lower()
+
+    return workers.text(
+        text_input = normalized_input, 
+        client = client,
+        assistant_id=ASSISTANT_ID,
+        thread_id = current_thread.id,
+        voice = voice
+    )
+
+async def describe_photo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat_id: str = str(update.message.chat.id)
+    current_thread = retrieve_thread(chat_id)
+    text = update.message.caption
+    media_files = update.message.photo
+    images = [await context.bot.getFile(media.file_id) for _,media in enumerate(media_files)]
+    image_paths = images[-1].file_path
+
+
+    # print(images[-1].file_path)
+
+async def accept_voice(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat_id: str = str(update.message.chat.id)
+    voice = await context.bot.getFile(update.message.voice.file_id)
+    voice_file = requests.get(voice.file_path)
+    voice_bytes = BytesIO(voice_file.content)
+    voice_bytes.name = 'voice.ogg'
+    voice_bytes.seek(0)
+    text_version = workers.transcribe_audio(voice_bytes, client = client)
+    current_thread = retrieve_thread(chat_id)
+    bot_response = generate_response(text_version, current_thread, voice = True)
+    await update.message.reply_voice(voice = bot_response)
+
+async def process_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat_type: str = update.message.chat.type
+    chat_id: str = str(update.message.chat.id)
+    text: str = update.message.text
+
+    current_thread = retrieve_thread(chat_id)
+
+    # Handle group messages only if bot is mentioned
+    if chat_type == 'group':
+        if BOT_HANDLE in text:
+            cleaned_text: str = text.replace(BOT_HANDLE, '').strip()
+            response: str = generate_response(cleaned_text, current_thread)
+        else:
+            return  # Ignore messages where bot is not mentioned in a group
+    else:
+        response: str = generate_response(text, current_thread)
+
+    # Reply to the user
+    # print('Bot response:', response)
+    await update.message.reply_text(response)
+
+
+# Log errors
+async def log_error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(f'Update {update} caused error {context.error}')
+
+
+# Start the bot
+if __name__ == '__main__':
+    app = Application.builder().token(TG_KEY).build()
+
+    # Register command handlers
+    app.add_handler(CommandHandler('help', assist_command))
+
+    # Register message handler
+    app.add_handler(MessageHandler(filters.TEXT, process_message))
+
+    app.add_handler(MessageHandler(filters.PHOTO, describe_photo))
+
+    app.add_handler(MessageHandler(filters.VOICE, accept_voice))
+
+    # Register error handler
+    app.add_error_handler(log_error)
+
+    print('Starting polling...')
+    # Run the bot
+    app.run_polling(poll_interval=2)
--- workers.py
+++ workers.py
@@ -1,13 +1,6 @@
-from openai import OpenAI
-import os
-
-
-ASSISTANT_ID = os.getenv('ASSISTANT_ID')
-OPENAI_KEY = os.getenv('AI_KEY')
-
-client = OpenAI(api_key = OPENAI_KEY)
-
-
+import time
+from typing import List
+from io import BytesIO
 def check_thread(chat_id, deta_base):
     result = deta_base.Base('chat_ids').fetch({"key":chat_id}).items
     if len(result)>0:
@@ -17,12 +10,45 @@ def check_thread(chat_id, deta_base):
 def push_thread(chat_id, thread_id, deta_base):
     deta_base.Base('chat_ids').put(key = chat_id, data = {'thread':thread_id})
 
-def text(text_input, client, assistant_id, thread_id):
-    messages = [{'role':'user','content':text_input}]
-    response = client.beta.thre
+def text(text_input, client, assistant_id, thread_id, voice = False):
+    messages = [{'type':'text','text':text_input}]
+    _ = client.beta.threads.messages.create(
+        thread_id = thread_id,
+        content = messages,
+        role = 'user'
+        )
+    current_run = client.beta.threads.runs.create(
+        thread_id=thread_id,
+        assistant_id=assistant_id
+        )
+    current_status = 'queued'
+    while current_status in ('queued','in_progress'):
+        time.sleep(0.5)
+        current_run = client.beta.threads.runs.retrieve(thread_id = thread_id, run_id = current_run.id)
+        current_status = current_run.status
+    messages = client.beta.threads.messages.list(thread_id)
+    response = messages.data[0].content[0].text.value
+    if not voice:
+        return response
+    else:
+        voice_response = client.audio.speech.create(
+        model="tts-1",
+        voice="nova",
+        input=response
+        )
 
-def image(image_input):
-    pass
+        voice_response = BytesIO(voice_response.content)
+        voice_response.name = 'response.ogg'
+    return voice_response
 
-def voice(voice_input):
-    pass
+def image(images: List, text: str, client, assistant_id: str, thread_id: str):
+    assert isinstance(images, list)
+    if not text:
+        text = 'What is in these images?'
+    messages = [{'type':'text','text':text}]
+    
+
+
+def transcribe_audio(voice_input, client):
+    response = client.audio.transcriptions.create(file = voice_input, model = 'whisper-1')
+    return response.text
